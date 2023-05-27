6c1a39dcde1b301c4d1af52f400f15ad76109167
Iago Medeiros <iagomedeiros.dev@gmail.com>
2023-05-27 19:33:33 -0300

    fixing node_modules

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,49 +1,49 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useMutation } from "@tanstack/react-query";
 
 async function fetchComments(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
-  );
-  return response.json();
+	const response = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`);
+	return response.json();
 }
 
 async function deletePost(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: 'DELETE' }
-  );
-  return response.json();
+	const response = await fetch(`https://jsonplaceholder.typicode.com/postId/${postId}`, {
+		method: "DELETE",
+	});
+	return response.json();
 }
 
 async function updatePost(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: 'PATCH', data: { title: 'REACT QUERY FOREVER!!!!' } }
-  );
-  return response.json();
+	const response = await fetch(`https://jsonplaceholder.typicode.com/postId/${postId}`, {
+		method: "PATCH",
+		data: { title: "REACT QUERY FOREVER!!!!" },
+	});
+	return response.json();
 }
 
+const { mutate } = useMutation((postId) => updatePost(postId));
+
 export function PostDetail({ post }) {
+	const { data, isError, error, isLoading } = useQuery(["fetchCom"], () =>
+		fetchComments(post.id),
+	);
 
-  const { data, isError, error, isLoading } = useQuery(['fetchCom'], () => fetchComments(post.id));
-
-  return (
-    <>
-      <h3 style={{ color: 'blue' }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
-      <p>{post.body}</p>
-      {isLoading ? (
-        <h1>Loading comments...</h1>
-      ) : (
-        <>
-          <h4>Comments</h4>
-          {data.map((comment) => (
-            <li key={comment.id}>
-              {comment.email}: {comment.body}
-            </li>
-          ))}
-        </>
-      )}
-    </>
-  );
+	return (
+		<>
+			<h3 style={{ color: "blue" }}>{post.title}</h3>
+			<button>Delete</button> <button onClick={mutate(post.postId)}>Update title</button>
+			<p>{post.body}</p>
+			{isLoading ? (
+				<h1>Loading comments...</h1>
+			) : (
+				<>
+					<h4>Comments</h4>
+					{data.map((comment) => (
+						<li key={comment.id}>
+							{comment.email}: {comment.body}
+						</li>
+					))}
+				</>
+			)}
+		</>
+	);
 }
